ccbc61a720efed81d5cee2bd4922d5644a723285
julian <jcvgalfonso@gmail.com>
2015-09-20 22:41:56 -0500

    [Verde] Se pueden recibir cadenas de mas de 1 numero

--- Solver.py
+++ Solver.py
@@ -6,3 +6,10 @@ def solve(cadena):
         return 0
     elif len(cadena) == 1:
         return cadena
+    elif len(cadena)>1:
+        numbers = cadena.split(',')
+        numbers = numbers[:2]
+        result=0
+        for number in numbers:
+            result += int(number)
+        return result
